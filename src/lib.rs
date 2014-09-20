f47bb789d35f3b2675157a2e01bee55617711a1a
Konstantin Stepanov <me@kstep.me>
2014-09-21 02:25:42 +0300

    reorganize flags

--- src/lib.rs
+++ src/lib.rs
@@ -62,20 +62,25 @@ static WEBSOCKET_GUID: &'static [u8] = b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 
 bitflags! {
     #[deriving(Show)] flags WSHeader: u16 {
-        static WS_FIN     = 0b1000000000000000,
-        static WS_OPCODE  = 0b0000111100000000,
-        static WS_MASK    = 0b0000000010000000,
-        static WS_LEN     = 0b0000000001111111,
+        // Main structure, mask with & to get header parts
+        static WS_FIN     = 0b1000000000000000, // final flag
+        static WS_RSV     = 0b0111000000000000, // reserved
+        static WS_OPCODE  = 0b0000111100000000, // opcode
+        static WS_MASK    = 0b0000000010000000, // mask flag
+        static WS_LEN     = 0b0000000001111111, // length
 
-        static WS_LEN16   = 0b0000000001111110,
-        static WS_LEN64   = 0b0000000001111111,
-
+        // Opcodes, check for equality after masking with WS_OPCODE
         static WS_OPCONT  = 0b0000000000000000,
         static WS_OPTEXT  = 0b0000000100000000,
         static WS_OPBIN   = 0b0000001000000000,
         static WS_OPTERM  = 0b0000100000000000,
         static WS_OPPING  = 0b0000100100000000,
         static WS_OPPONG  = 0b0000101000000000,
+
+        // Helper masks
+        static WS_OPCTRL  = 0b0000100000000000, // if matches with &, this is a control code
+        static WS_LEN16   = 0b0000000001111110, // if &WS_LEN equals this, it has 16-bit length
+        static WS_LEN64   = 0b0000000001111111, // if &WS_LEN equals this, it has 32-bit length
     }
 }
 
