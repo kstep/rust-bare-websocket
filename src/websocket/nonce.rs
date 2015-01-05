0c9f0c0181e8e054cafb22a90525e9f0b7a1ea4f
Konstantin Stepanov <me@kstep.me>
2015-01-05 16:14:43 +0300

    slice trait for nonce

--- src/websocket/nonce.rs
+++ src/websocket/nonce.rs
@@ -1,4 +1,5 @@
 use std::rand::{self, Rng};
+use std::ops::Slice;
 use serialize::base64::{self, ToBase64};
 use crypto::sha1::Sha1;
 use crypto::digest::Digest;
@@ -15,7 +16,7 @@ impl Nonce {
 
     fn generate(r: &mut Rng) -> Nonce {
         let mut nonce = [0u8; 10];
-        r.fill_bytes(nonce);
+        r.fill_bytes(nonce.as_mut_slice());
         Nonce(nonce.to_base64(base64::STANDARD))
     }
 
@@ -25,13 +26,31 @@ impl Nonce {
         let mut result = [0u8; 20];
         sha1.input(n.as_bytes());
         sha1.input(WEBSOCKET_GUID);
-        sha1.result(result);
+        sha1.result(result.as_mut_slice());
         Nonce(result.to_base64(base64::STANDARD))
     }
 }
 
-impl<'a> Str for Nonce {
-    fn as_slice<'a>(&'a self) -> &'a str {
+impl Slice<uint, str> for Nonce {
+    #[inline] fn as_slice_(&self) -> &str {
         match *self { Nonce(ref n) => n[] }
+    }
+
+    #[inline] fn slice_from_or_fail(&self, from: &uint) -> &str {
+        match *self { Nonce(ref n) => n[*from..] }
+    }
+
+    #[inline] fn slice_to_or_fail(&self, to: &uint) -> &str {
+        match *self { Nonce(ref n) => n[..*to] }
+    }
+
+    #[inline] fn slice_or_fail(&self, from: &uint, to: &uint) -> &str {
+        match *self { Nonce(ref n) => n[*from..*to] }
+    }
+}
+
+impl Str for Nonce {
+    fn as_slice<'a>(&'a self) -> &'a str {
+        self[]
     }
 }
