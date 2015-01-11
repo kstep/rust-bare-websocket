60553fd2ac3da7147a74a3ed89b38dd204afd8c8
Konstantin Stepanov <me@kstep.me>
2015-01-11 14:02:21 +0300

    example update

--- examples/echo.rs
+++ examples/echo.rs
@@ -1,6 +1,7 @@
 #![feature(slicing_syntax)]
 
-extern crate "bare-websocket" as websocket;
+//extern crate "bare-websocket" as websocket;
+extern crate websocket;
 extern crate url;
 
 use websocket::{WebSocket, WSMessage};
@@ -8,23 +9,22 @@ use url::Url;
 
 fn main() {
     let url = Url::parse("ws://echo.websocket.org").unwrap();
-    let mut ws = WebSocket::with_options(url, 13, Some(&["chat", "superchat"]), None);
+    let mut ws = WebSocket::with_options(url, 13, Some(&["chat", "superchat"][]), None);
     ws.connect().unwrap();
 
     let msg = WSMessage::text("Hello, World!"); //.mask();
-    b.bytes = msg.data.len() as u64;
-    
+
     ws.send_message(&msg);
 
     for m in WSMessage::text("Hello, World!").split(5) {
-        println!("msg: {} {}", m, m.to_string());
+        println!("msg: {:?} {:?}", m, m.to_string());
         ws.send_message(&m).unwrap();
     }
 
     let reply = ws.iter().defrag().next();//.unwrap();
-    println!("received: {}", reply);
+    println!("received: {:?} {:?}", reply, reply.as_ref().map(|v| v.to_string()));
 
     for msg in ws.iter() {
-        println!("{}", msg.to_string());
+        println!("{:?}", msg.to_string());
     }
 }
